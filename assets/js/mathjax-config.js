fe2154470bfa1cd72090cbf4f91a665ecb4e2421
Pranav Sharma <psharma@outlook.com>
2020-06-02 13:04:13 +1000

    Add mhchem

--- assets/js/mathjax-config.js
+++ assets/js/mathjax-config.js
@@ -14,3 +14,10 @@ window.MathJax = {
     load: ['[tex]/noerrors']
   }
 };
+MathJax.Ajax.config.path["mhchem"] =
+  "https://cdnjs.cloudflare.com/ajax/libs/mathjax-mhchem/3.3.2";
+MathJax.Hub.Config({
+  TeX: {
+    extensions: ["[mhchem]/mhchem.js"]
+  }
+});
